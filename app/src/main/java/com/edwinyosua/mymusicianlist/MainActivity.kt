4842d608573ee363e1e6dfd6ab17c652af0e72dc
Edwin Yosua <115564225+EdwinYosua@users.noreply.github.com>
2024-12-29 14:16:55 +0800

    small finishing

--- app/src/main/java/com/edwinyosua/mymusicianlist/MainActivity.kt
+++ app/src/main/java/com/edwinyosua/mymusicianlist/MainActivity.kt
@@ -3,14 +3,25 @@ package com.edwinyosua.mymusicianlist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.runtime.Composable
 import com.edwinyosua.mymusicianlist.data.model.Musician
 import com.edwinyosua.mymusicianlist.ui.menus.HomeScreen
+import com.edwinyosua.mymusicianlist.ui.menus.MusicianProfileActivity
+import com.edwinyosua.mymusicianlist.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { MainApp{} }
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO) //disable night mode in app
+
+        setContent {
+//            AppTheme {
+                MainApp {
+                    startActivity(MusicianProfileActivity.newIntent(this, it))
+                }
+//            }
+        }
     }
 }
 
